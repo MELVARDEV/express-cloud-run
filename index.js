84429cf3550007e40f8b103754cd3597cc899e36
MELVAR <melvar@exory.dev>
2022-08-10 17:25:09 +0200

    updated cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 var bodyParser = require('body-parser')
 const mongoose = require("mongoose");
 require('dotenv').config()
+
+app.use(cors());
 
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
   console.log("Connected to the database");
